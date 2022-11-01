48ba49c9b8c59868b699e9293c376fd145d0eaf8
TaylanTorres09 <taylantorres@gmail.com>
2022-11-01 20:48:31 -0300

    Create functionality of payment

--- Assets/JS/pagamento.js
+++ Assets/JS/pagamento.js
@@ -7,3 +7,29 @@ const total = localStorage.getItem("total");
 const totalNumber = parseFloat(total).toFixed(2).replace(".", ",");
 document.getElementById("totalPagar").innerText= "R$ " + totalNumber;
 
+async function pagamento() {
+    // const url = "http://localhost:8080";
+    const url = "https://api-menupizzaria.herokuapp.com";
+  
+    const orderId = localStorage.getItem("orderId");
+    
+    const response = await fetch(`${url}/payment/register/${orderId}`, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        mode: "cors",
+    });
+    const message = await response.json();
+    if(response.status === 201) {
+        localStorage.removeItem("product")
+        localStorage.removeItem("total")
+        localStorage.removeItem("orderId")
+        alert("Pagamento feito com sucesso!");
+        window.location.href = "home.html";
+    } else {
+        alert(message.mensage);
+    }
+       
+  }
